2c2c5d218aaa68d7bb22902e9a7317e2ea8542ab
Philippe M. Chiasson <gozer@ectoplasm.org>
2019-12-03 21:15:38 -0500

    ignore flux managed attributes

--- flux.tf
+++ flux.tf
@@ -2,5 +2,12 @@ resource "kubernetes_namespace" "flux" {
   metadata {
     name = "flux-system"
   }
+
+  lifecycle {
+    ignore_changes = [
+        "metadata.0.labels",
+        "metadata.0.annotations",
+    ]
+}
 }
 
