e7f9f031de982e40aeaf3afba1050ee690cec102
Philippe M. Chiasson <gozer@ectoplasm.org>
2019-12-10 21:21:48 -0500

    Reseal secrets for correct namespaces

--- k8s/app/common/seal.sh
+++ k8s/app/common/seal.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-aws-vault exec appsvcs-voice-admin -- kubeseal -o yaml < secret.yaml.encrypted > secret.yaml
+DIR=$(dirname "$0")
+NAMESPACE=$(yq r - namespace < "$DIR/kustomization.yaml")
+
+aws-vault exec appsvcs-voice-admin -- kubeseal --namespace "$NAMESPACE" -o yaml < secret.yaml.encrypted > secret.yaml
