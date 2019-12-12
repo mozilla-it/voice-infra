caed6ebb09f01011131c2b33cefb6d507f21c803
Philippe M. Chiasson <gozer@ectoplasm.org>
2019-12-12 11:08:52 -0500

    dont run helm hooks

--- k8s/app/common/elasticsearch/generate.sh
+++ k8s/app/common/elasticsearch/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-helm template elasticsearch elastic/elasticsearch --dry-run --values values.yaml | grep -Ev ' *namespace:' > resources.yaml
+helm template elasticsearch elastic/elasticsearch --no-hooks --version 7.5.0 --values values.yaml | grep -Ev ' *namespace:' > resources.yaml
