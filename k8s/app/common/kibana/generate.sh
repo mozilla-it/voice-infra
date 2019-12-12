ba9e220c33307e702a040a3f431bc1aae3a17745
Philippe M. Chiasson <gozer@ectoplasm.org>
2019-12-12 13:50:58 -0500

    rename kibana

--- k8s/app/common/kibana/generate.sh
+++ k8s/app/common/kibana/generate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-helm template kibana elastic/kibana --no-hooks --version 7.5.0 --values values.yaml | grep -Ev ' *namespace:' > resources.yaml
+helm template kibana elastic/kibana --no-hooks --version 7.5.0 --values values.yaml | grep -Ev ' *namespace:' | sed -e's/kibana-kibana/kibana/g' > resources.yaml
