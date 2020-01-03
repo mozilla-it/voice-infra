c0d1352436a94f0125ab1fa36ae41a1619e6d004
Philippe M. Chiasson <gozer@ectoplasm.org>
2020-01-03 14:49:03 -0500

    new ssl certs with SANs

--- t7m/acm.tf
+++ t7m/acm.tf
@@ -1,5 +1,10 @@
 resource "aws_acm_certificate" "dev" {
-  domain_name       = "voice-dev.allizom.org"
+  domain_name = "voice-dev.allizom.org"
+
+  subject_alternative_names = [
+    "dev.voice.mozit.cloud",
+  ]
+
   validation_method = "DNS"
 
   tags {
@@ -14,29 +19,53 @@ resource "aws_acm_certificate" "dev" {
 resource "aws_acm_certificate" "sandbox" {
   domain_name = "voice-sandbox.allizom.org"
 
+  subject_alternative_names = [
+    "sandbox.voice.mozit.cloud",
+  ]
+
   tags {
     Name = "voice-k8s-sandbox"
   }
 
   validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_acm_certificate" "stage" {
   domain_name = "voice.allizom.org"
+
+  subject_alternative_names = [
+    "stage.voice.mozit.cloud",
+  ]
 
   tags {
     Name = "voice-k8s-stage"
   }
 
   validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_acm_certificate" "prod" {
   domain_name = "voice.mozilla.org"
+
+  subject_alternative_names = [
+    "prod.voice.mozit.cloud",
+  ]
 
   tags {
     Name = "voice-k8s-prod"
   }
 
   validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
