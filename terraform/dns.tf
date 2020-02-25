e8e2dd609813c35a8bf27763afddc976c6cb0eb7
Alberto del Barrio <The-smooth-operator@users.noreply.github.com>
2020-02-25 15:34:27 +0100

    Point Sandbox DNS to new cluster (#18)

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -2,7 +2,7 @@ data "aws_route53_zone" "voice_mozit_cloud" {
   name = "voice.mozit.cloud"
 }
 
-data "aws_elb" "dev_elb" {
+data "aws_elb" "dev" {
   name = "a173022d857d011eabf210a8061d9461"
 }
 
@@ -12,8 +12,24 @@ resource "aws_route53_record" "dev" {
   type    = "A"
 
   alias {
-    name                   = "dualstack.${data.aws_elb.dev_elb.dns_name}"
-    zone_id                = data.aws_elb.dev_elb.zone_id
+    name                   = "dualstack.${data.aws_elb.dev.dns_name}"
+    zone_id                = data.aws_elb.dev.zone_id
+    evaluate_target_health = false
+  }
+}
+
+data "aws_elb" "sandbox" {
+  name = "a244190cd57d811eabf210a8061d9461"
+}
+
+resource "aws_route53_record" "sandbox" {
+  zone_id = data.aws_route53_zone.voice_mozit_cloud.zone_id
+  name    = "sandbox.voice.mozit.cloud"
+  type    = "A"
+
+  alias {
+    name                   = "dualstack.${data.aws_elb.sandbox.dns_name}"
+    zone_id                = data.aws_elb.sandbox.zone_id
     evaluate_target_health = false
   }
 }
