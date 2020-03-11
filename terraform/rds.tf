428794ae848875a62e52ed51a85ae692ca0178c1
Alberto del Barrio <adelbarrio@mozilla.com>
2020-03-11 12:57:59 +0100

    Create new deployment for stage

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,10 +1,31 @@
+module "db-stage" {
+  source                  = "github.com/mozilla-it/terraform-modules//aws/database"
+  type                    = "mysql"
+  name                    = "voice"
+  username                = "admin"
+  identifier              = "voice-eks-stage"
+	instance                = "db.t3.small"
+  storage_gb              = "32"
+  db_version              = "5.6"
+  multi_az                = "false"
+  vpc_id                  = module.vpc.vpc_id
+  subnets                 = module.vpc.private_subnets.0
+  parameter_group_name    = aws_db_parameter_group.voice_parameters.name
+  backup_retention_period = 3
+  replica_enabled         = "true"
+
+  cost_center = "1003"
+  project     = "voice"
+  environment = "stage"
+}
+
 module "db-dev" {
-  # TODO change to upstream once PR #18 is merged
-  source                  = "github.com/mozilla-it/terraform-modules//aws/database?ref=6fc2eb44d50d6da4f854ea9954c317537e7c7504"
+  source                  = "github.com/mozilla-it/terraform-modules//aws/database"
   type                    = "mysql"
   name                    = "voice"
   username                = "voice"
   identifier              = "voice-eks-dev"
+	instance                = "db.t3.small"
   storage_gb              = "35"
   db_version              = "5.6"
   multi_az                = "false"
@@ -20,12 +41,12 @@ module "db-dev" {
 }
 
 module "db-sandbox" {
-  # TODO change to upstream once PR #18 is merged
-  source                  = "github.com/mozilla-it/terraform-modules//aws/database?ref=6fc2eb44d50d6da4f854ea9954c317537e7c7504"
+  source                  = "github.com/mozilla-it/terraform-modules//aws/database"
   type                    = "mysql"
   name                    = "voice"
   username                = "voice"
   identifier              = "voice-eks-sandbox"
+	instance                = "db.t3.small"
   storage_gb              = "35"
   db_version              = "5.6"
   multi_az                = "false"
