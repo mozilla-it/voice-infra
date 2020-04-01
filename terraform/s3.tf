8affda8498a46d7c3953195b86fec3a968c5e5d3
Alberto del Barrio <The-smooth-operator@users.noreply.github.com>
2020-04-01 11:03:46 +0200

    Alberto/s3 buckets (#44)
    * Set the bucket name
    
    * Manage S3 buckets for clips upload

---
+++ terraform/s3.tf
@@ -0,0 +1,17 @@
+data "aws_s3_bucket" "voice-prod" {
+  bucket = "voice-prod-clips-393eefd0cba28c270ced0f9587a4f6ae601ca91e"
+}
+
+data "aws_s3_bucket" "voice-stage" {
+  bucket = "voice-stage-clips-934a7753ac94c18242c82ed71d5088b24b02bdd6"
+}
+
+resource "aws_s3_bucket" "voice-dev" {
+  bucket = "voice-dev-clips-20191211013626583200000002"
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket" "voice-sandbox" {
+  bucket = "voice-sandbox-clips-20191211013626583100000001"
+  acl    = "public-read"
+}
