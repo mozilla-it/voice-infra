66da05fa50ca05b6282c93bb81b8a0587f9b8ad0
Alberto del Barrio <adelbarrio@mozilla.com>
2020-04-03 10:07:16 +0200

    Deploy ES clusters only for stage and prod

--- terraform/elasticsearch.tf
+++ terraform/elasticsearch.tf
@@ -1,18 +1,19 @@
 module "elasticsearch-stage" {
-	source                  = "github.com/mozilla-it/terraform-modules//aws/elasticsearch?ref=f59a68b3cd14c441c5c386bdb0023c3dbd8ab120"
+	source                  = "github.com/mozilla-it/terraform-modules//aws/elasticsearch"
 	domain_name             = "voice-eks-stage"
 	subnet_ids              = [element(module.vpc.private_subnets[0], 0)]
 	vpc_id                  = module.vpc.vpc_id
 	ingress_security_groups = "sg-0b42a7cfbb3ccb3bc"
-	ebs_volume_size         = 35
+	ebs_volume_size         = 80
+	es_instance_type        = "m3.medium.elasticsearch"
 }
 
-module "elasticsearch-sandbox" {
-	source                  = "github.com/mozilla-it/terraform-modules//aws/elasticsearch?ref=f59a68b3cd14c441c5c386bdb0023c3dbd8ab120"
-	domain_name             = "voice-eks-sandbox"
+module "elasticsearch-prod" {
+	source                  = "github.com/mozilla-it/terraform-modules//aws/elasticsearch"
+	domain_name             = "voice-eks-prod"
 	subnet_ids              = [element(module.vpc.private_subnets[0], 0)]
 	vpc_id                  = module.vpc.vpc_id
 	ingress_security_groups = "sg-0b42a7cfbb3ccb3bc"
-	ebs_volume_size         = 10
-	tags                    = var.common-tags
+	ebs_volume_size         = 80
+	es_instance_type        = "m3.medium.elasticsearch"
 }
