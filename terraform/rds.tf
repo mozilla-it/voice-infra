6dab4a3da4c73c11b754ab30b95748776241f750
Alberto del Barrio <adelbarrio@mozilla.com>
2020-04-08 15:37:18 +0200

    Enable performance insights on database.
    In order to enable it, I had to upgrade the databases from 5.6.40 to 5.6.46. Also I did create 2 PRs in the upstream terraform module for allow setting these parameters

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,19 +1,22 @@
 module "db-stage" {
-  source                  = "github.com/mozilla-it/terraform-modules//aws/database"
-  type                    = "mysql"
-  name                    = "voice"
-  username                = "admin"
-  identifier              = "voice-eks-stage"
-  instance                = "db.t3.medium"
-  storage_gb              = "50"
-  db_version              = "5.6"
-  multi_az                = "false"
-  vpc_id                  = module.vpc.vpc_id
-  subnets                 = module.vpc.private_subnets.0
-  parameter_group_name    = aws_db_parameter_group.voice_parameters.name
-  backup_retention_period = 3
-  replica_enabled         = "true"
-	instance_replica        = "db.t3.small"
+  source                         = "github.com/mozilla-it/terraform-modules//aws/database"
+  type                           = "mysql"
+  name                           = "voice"
+  username                       = "admin"
+  identifier                     = "voice-eks-stage"
+  instance                       = "db.t3.medium"
+  storage_gb                     = "50"
+  db_version                     = "5.6.46"
+  multi_az                       = "false"
+  vpc_id                         = module.vpc.vpc_id
+  subnets                        = module.vpc.private_subnets.0
+  parameter_group_name           = aws_db_parameter_group.voice_parameters.name
+  backup_retention_period        = 3
+  replica_enabled                = "true"
+  instance_replica               = "db.t3.small"
+  replica_db_version             = "5.6.46"
+  performance_insights_enabled   = "true"
+  performance_insights_retention = 30
 
   cost_center = "1003"
   project     = "voice"
@@ -21,21 +24,24 @@ module "db-stage" {
 }
 
 module "db-dev" {
-  source                  = "github.com/mozilla-it/terraform-modules//aws/database"
-  type                    = "mysql"
-  name                    = "voice"
-  username                = "voice"
-  identifier              = "voice-eks-dev"
-  instance                = "db.t3.small"
-  storage_gb              = "35"
-  db_version              = "5.6"
-  multi_az                = "false"
-  vpc_id                  = module.vpc.vpc_id
-  subnets                 = module.vpc.private_subnets.0
-  parameter_group_name    = aws_db_parameter_group.voice_parameters.name
-  backup_retention_period = 1
-  replica_enabled         = "true"
-	instance_replica        = "db.t3.small"
+  source                         = "github.com/mozilla-it/terraform-modules//aws/database?ref=57920329349a95abdad3305afa883f30822e484e"
+  type                           = "mysql"
+  name                           = "voice"
+  username                       = "voice"
+  identifier                     = "voice-eks-dev"
+  instance                       = "db.t3.small"
+  storage_gb                     = "35"
+  db_version                     = "5.6.46"
+  multi_az                       = "false"
+  vpc_id                         = module.vpc.vpc_id
+  subnets                        = module.vpc.private_subnets.0
+  parameter_group_name           = aws_db_parameter_group.voice_parameters.name
+  backup_retention_period        = 1
+  replica_enabled                = "true"
+  instance_replica               = "db.t3.small"
+  replica_db_version             = "5.6.46"
+  performance_insights_enabled   = "true"
+  performance_insights_retention = 7
 
   cost_center = "1003"
   project     = "voice"
@@ -43,21 +49,22 @@ module "db-dev" {
 }
 
 module "db-sandbox" {
-  source                  = "github.com/mozilla-it/terraform-modules//aws/database"
+  source                  = "github.com/mozilla-it/terraform-modules//aws/database?ref=57920329349a95abdad3305afa883f30822e484e"
   type                    = "mysql"
   name                    = "voice"
   username                = "voice"
   identifier              = "voice-eks-sandbox"
   instance                = "db.t3.small"
   storage_gb              = "35"
-  db_version              = "5.6"
+  db_version              = "5.6.46"
   multi_az                = "false"
   vpc_id                  = module.vpc.vpc_id
   subnets                 = module.vpc.private_subnets.0
   parameter_group_name    = aws_db_parameter_group.voice_parameters.name
   backup_retention_period = 1
   replica_enabled         = "true"
-	instance_replica        = "db.t3.small"
+  instance_replica        = "db.t3.small"
+  replica_db_version      = "5.6.46"
 
   cost_center = "1003"
   project     = "voice"
