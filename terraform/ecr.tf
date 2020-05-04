86ad357a1b2d2efc74c877895f151c4518f30b9e
Alberto del Barrio <adelbarrio@mozilla.com>
2020-05-04 15:11:01 +0200

    Add a registry for go-mysql-elasticsearch containers

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -6,3 +6,11 @@ resource "aws_ecr_repository" "ecr" {
   }
 }
 
+resource "aws_ecr_repository" "go-mysql-elasticsearch" {
+  name                 = "go-mysql-elasticsearch"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
