19bc73e2c4dccb6ea79d3a497a63472f8fa22b0f
Alberto del Barrio <adelbarrio@mozilla.com>
2020-05-12 11:46:54 +0200

    Create DNS entry for Grafana

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -65,3 +65,15 @@ resource "aws_route53_record" "sandbox" {
 data "aws_route53_zone" "voice_mozit_cloud" {
   name = "voice.mozit.cloud"
 }
+
+resource "aws_route53_record" "stats" {
+  zone_id = data.aws_route53_zone.voice_mozit_cloud.zone_id
+  name    = "stats.voice.mozit.cloud"
+  type    = "A"
+
+  alias {
+    name                   = data.aws_elb.stage.dns_name
+    zone_id                = data.aws_elb.stage.zone_id
+    evaluate_target_health = false
+  }
+}
