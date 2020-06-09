486323cd56881b32ec1b6343b221dc208b22be9d
Alberto del Barrio <adelbarrio@mozilla.com>
2020-06-09 14:22:02 +0200

    Use logger container from ECR in Dev

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ecr" {
-  name                 = "voice"
+  name = "voice"
 
   image_scanning_configuration {
     scan_on_push = true
@@ -7,7 +7,7 @@ resource "aws_ecr_repository" "ecr" {
 }
 
 resource "aws_ecr_repository" "go-mysql-elasticsearch" {
-  name                 = "go-mysql-elasticsearch"
+  name = "go-mysql-elasticsearch"
 
   image_scanning_configuration {
     scan_on_push = true
@@ -15,9 +15,15 @@ resource "aws_ecr_repository" "go-mysql-elasticsearch" {
 }
 
 resource "aws_ecr_repository" "common-voice-bundler" {
-  name                 = "common-voice-bundler"
+  name = "common-voice-bundler"
 
   image_scanning_configuration {
     scan_on_push = true
   }
+}
+
+module "ecr" {
+  source      = "github.com/mozilla-it/terraform-modules//aws/ecr?ref=5df4a370339b04193938a5d3455015c0e1c6b247"
+  repo_name   = "remote_syslog2"
+  create_user = false
 }
