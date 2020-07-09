393ada3a17090e89fb1f68d2369cf10469846d0a
limed <limed@mozilla.com>
2020-07-09 14:32:29 -0700

    Switch policy_arn input to a list

--- terraform/maws.tf
+++ terraform/maws.tf
@@ -2,14 +2,18 @@ module "devs_ro" {
   source       = "github.com/mozilla-it/terraform-modules//aws/maws-roles?ref=master"
   role_name    = "maws-devs-ro"
   role_mapping = ["voice-dev"]
-  policy_arn   = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+  policy_arn = [
+    "arn:aws:iam::aws:policy/ReadOnlyAccess"
+  ]
 }
 
 module "devs_role" {
   source       = "github.com/mozilla-it/terraform-modules//aws/maws-roles?ref=master"
   role_name    = "maws-devs-rw"
   role_mapping = ["voice-dev"]
-  policy_arn   = aws_iam_policy.devs_policy.arn
+  policy_arn = [
+    aws_iam_policy.devs_policy.arn
+  ]
 }
 
 resource "aws_iam_policy" "devs_policy" {
@@ -29,7 +33,8 @@ data "aws_iam_policy_document" "devs_policy" {
   statement {
     sid       = "s3ObjectLevelPermissions"
     resources = ["arn:aws:s3:::voice-*"]
-    actions = ["s3:AbortMultipartUpload",
+    actions = [
+      "s3:AbortMultipartUpload",
       "s3:DeleteObject",
       "s3:DeleteObjectTagging",
       "s3:DeleteObjectVersion",
@@ -48,12 +53,16 @@ data "aws_iam_policy_document" "devs_policy" {
       "s3:PutObjectTagging",
       "s3:PutObjectVersionAcl",
       "s3:PutObjectVersionTagging",
-    "s3:RestoreObject"]
+      "s3:RestoreObject"
+    ]
   }
 
   statement {
     sid       = "s3ListBucket"
     resources = ["arn:aws:s3:::*"]
-    actions   = ["s3:ListAllMyBuckets", "s3:ListBucket"]
+    actions = [
+      "s3:ListAllMyBuckets",
+      "s3:ListBucket"
+    ]
   }
 }
